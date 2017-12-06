f962e1f8aa997bbcf6a9c3862b1141effe30b977
ValorMorgan <scriptcoded@hotmail.com>
2017-12-06 16:41:09 -0500

    Day 3 WIP

---
+++ Day3/Day3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Day3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day3")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b95f6a8-dc4f-4320-8988-40b817f23ac1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Day3/Day3/StepCount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Day3
+{
+    [TestFixture]
+    public class StepCount
+    {
+        private string _input = "325489";
+        private string _sample = "31";
+        
+        /* 
+         * 17 16 15 14 13
+         * 18  5  4  3 12
+    PORT * 19  6  1  2 11
+         * 20  7  8  9 10
+         * 21 22 23 -> ..
+         */
+        
+        [Test]
+        public void Steps_To_Carry_To_Port()
+        {
+            int input = int.Parse(_sample);
+            int stepCount = 0;
+            Console.WriteLine(input);
+
+            stepCount += input / 8 - 1;
+            input = input - (int)(8 * (int)(input / 8)) + 1;
+
+            Console.WriteLine($"Jumped to {input} in {stepCount} steps");
+
+            if (input > 1)
+            {
+                stepCount += (input - 1) % 2 == 0 ? 2 : 1;
+
+                Console.WriteLine("step: " + 1);
+            }
+            
+            Console.WriteLine($"Steps: {stepCount}");
+        }
+    }
+}
