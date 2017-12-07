7f890810eeaece0a1ab90ac9ab92aae7e4005dca
ValorMorgan <scriptcoded@hotmail.com>
2017-12-07 16:34:00 -0500

    Day 6 WIP

---
+++ Day6/Day6/MemoryReallocation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Day6
+{
+    [TestFixture]
+    public class MemoryReallocation
+    {
+        private readonly int[] _input =
+        {
+            4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3
+        };
+
+        private readonly int[] _sample =
+        {
+            0, 2, 7, 0
+        };
+
+        [Test]
+        public void Redistribution_Cycles_Until_Infinite()
+        {
+            int[] banks = _sample;
+            
+            IList<int[]> seen = new List<int[]> { banks };
+
+            do
+            {
+                int index = Array.IndexOf(banks, banks.Max());
+                int bank = banks[index];
+                banks[index] = 0;
+
+                while (bank > 0)
+                {
+                    index = index == banks.Length - 1 ?
+                        0 : index + 1;
+
+                    banks[index] += 1;
+                    bank -= 1;
+                }
+
+                seen.Add(banks);
+            }
+            while (!seen.Contains(banks));
+
+            Console.WriteLine($"Redistribution Cycles: {seen.Count - 1}");
+        }
+    }
+}
---
+++ Day6/Day6/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Day6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day6")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30b4322f-71d1-412f-bf43-3818013ee44e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
