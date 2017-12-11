f98c2b0cf9f13a6055033508484bcb18272e7a21
ValorMorgan <scriptcoded@hotmail.com>
2017-12-11 15:29:34 -0500

    Day 10 P2

--- AdventOfCode2017/AdventOfCode2017/Days/Day10.cs
+++ AdventOfCode2017/AdventOfCode2017/Days/Day10.cs
@@ -6,25 +6,66 @@ namespace AdventOfCode2017.Days
 {
     public class Day10 : IDay
     {
-        private readonly IEnumerable<int> _input = new List<int>()
-        {
-            94, 84, 0, 79, 2, 27, 81, 1, 123, 93, 218, 23, 103, 255, 254, 243
-        };
+        private readonly string _input = 
+            "94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243";
 
-        private readonly IEnumerable<int> _sample = new List<int>()
-        {
-            3, 4, 1, 5
-        };
+        private readonly string _sample =
+            "3,4,1,5";
+
+        private readonly string _sample2 =
+            "1,2,3";
         
         public void Part1()
         {
             Console.WriteLine("Knot Hash P.1");
 
+            IEnumerable<int> lengths = _input.Split(',')
+                .Select(l => int.Parse(l.Trim()));
+            
             int[] numbers = Enumerable.Range(0, 256).ToArray();
             int index = 0;
             int skipSize = 0;
+
+            DoRound(numbers, lengths, ref index, ref skipSize);
+
+            Console.WriteLine($"Hash Result: {numbers[0] * numbers[1]}");
+        }
+
+        public void Part2()
+        {
+            Console.WriteLine("Knot Hash P.2");
+
+            List<int> lengths = _input
+                .Select(c => (int)c)
+                .ToList();
             
-            foreach (int length in _input)
+            lengths.AddRange(new [] { 17, 31, 73, 47, 23 });
+
+            int[] numbers = Enumerable.Range(0, 256).ToArray();
+            int index = 0;
+            int skipSize = 0;
+
+            for (int _ = 1; _ <= 64; _++)
+                DoRound(numbers, lengths, ref index, ref skipSize);
+            
+            IEnumerable<int> denseHash = GetDenseHash(numbers).ToList();
+
+            Console.WriteLine(string.Join("", denseHash.Select(d => d.ToString("x2"))));
+        }
+
+        private IEnumerable<int> GetDenseHash(IEnumerable<int> numbers)
+        {
+            int blockSize = 16;
+            for (int i = 0; i * blockSize < numbers.Count(); i++)
+            {
+                IEnumerable<int> block = numbers.Skip(i * blockSize).Take(blockSize);
+                yield return block.Aggregate((a, b) => a^b);
+            }
+        }
+
+        private void DoRound(int[] numbers, IEnumerable<int> lengths, ref int index, ref int skipSize)
+        {
+            foreach (int length in lengths)
             {
                 IList<int> sublist = GetSublist(numbers, index, length);
 
@@ -36,20 +77,9 @@ namespace AdventOfCode2017.Days
                         index = 0;
                 }
 
-                index += skipSize;
-                if (index >= numbers.Length)
-                    index = index - numbers.Length;
+                index = (index + skipSize) % numbers.Length;
                 skipSize += 1;
             }
-
-            Console.WriteLine($"Hash Result: {numbers[0] * numbers[1]}");
-        }
-
-        public void Part2()
-        {
-            Console.WriteLine("Knot Hash P.2");
-            
-            throw new NotImplementedException();
         }
 
         private static IList<int> GetSublist(int[] numbers, int index, int length)
